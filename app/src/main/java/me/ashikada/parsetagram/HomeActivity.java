f7630fdc38ccaaec2a0b3997c42913f74faf7ecb
Alison Shikada <ashikada@fb.com>
2018-07-10 17:02:49 -0700

    finish video 4

--- app/src/main/java/me/ashikada/parsetagram/HomeActivity.java
+++ app/src/main/java/me/ashikada/parsetagram/HomeActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseQuery;
 
 import java.util.List;
 
@@ -23,13 +22,16 @@ public class HomeActivity extends AppCompatActivity {
         postsQuery.getTop().withUser();
 
 
-        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+        postsQuery.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> objects, ParseException e) {
                 if(e == null){
 
                     for(int i = 0; i< objects.size();i++){
-                        Log.d("HomeActivity", "Post[" + i + "] = "+ objects.get(i).getDescription());
+                        Log.d("HomeActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername()
+                        );
                     }
 
                 }
