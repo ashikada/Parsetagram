d925efdd955f049c127fafc9c3841e43ec73dad0
Alison Shikada <ashikada@fb.com>
2018-07-13 16:21:09 -0700

    added dialogue

--- app/src/main/java/me/ashikada/parsetagram/TimelineActivity.java
+++ app/src/main/java/me/ashikada/parsetagram/TimelineActivity.java
@@ -1,15 +1,19 @@
 package me.ashikada.parsetagram;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +25,9 @@ public class TimelineActivity extends AppCompatActivity {
     PostAdapter postAdapter;
     ArrayList<Post> posts;
     RecyclerView rvPosts;
+    private Button logOutBtn;
     private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class TimelineActivity extends AppCompatActivity {
         posts = new ArrayList<>();
         postAdapter = new PostAdapter(posts);
         rvPosts = (RecyclerView) findViewById(R.id.rvPosts);
+        logOutBtn = findViewById(R.id.logout_btn);
 
         //RecyclerView setup (layout manager, use adapter)
         rvPosts.setLayoutManager(new LinearLayoutManager(this));
@@ -51,6 +58,34 @@ public class TimelineActivity extends AppCompatActivity {
                 rvPosts.scrollToPosition(0);
             }
         });
+
+        logOutBtn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(TimelineActivity.this)
+                        .setMessage("Do you want to log out?")
+                        .setTitle("Log Out?")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ParseUser currentUser = ParseUser.getCurrentUser();
+                                currentUser.logOut();
+                                Intent intent = new Intent(TimelineActivity.this, MainActivity.class);
+                                startActivity(intent);
+
+                                //close the dialog
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        }).show();
+            }
+        });
+
 
         loadTopPosts();
     }
@@ -82,6 +117,7 @@ public class TimelineActivity extends AppCompatActivity {
         Intent i = new Intent(TimelineActivity.this, PostActivity.class);
         startActivity(i);
     }
+
 
 
 }
